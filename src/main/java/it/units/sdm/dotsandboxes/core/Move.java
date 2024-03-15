8012c3d617534716c145f73a4064c7fda3a9b6a2
simdeistud <simone.deiana@studenti.units.it>
2024-03-15 21:09:14 +0100

    Changed from record to class in order to impose the correct constructor

--- src/main/java/it/units/sdm/dotsandboxes/core/Move.java
+++ src/main/java/it/units/sdm/dotsandboxes/core/Move.java
@@ -1,4 +1,19 @@
 package it.units.sdm.dotsandboxes.core;
 
-public record Move(Player player, Integer lineHash) {
+public class Move {
+    private final Player player;
+    private final int uncoloredNormalizedLineHash;
+
+    public Move(Player player, Line line) {
+        this.player = player;
+        uncoloredNormalizedLineHash = LineUtils.stripOfColor(LineUtils.normalize(line)).hashCode();
+    }
+
+    public Player player() {
+        return player;
+    }
+
+    public int uncoloredNormalizedLineHash() {
+        return uncoloredNormalizedLineHash;
+    }
 }
